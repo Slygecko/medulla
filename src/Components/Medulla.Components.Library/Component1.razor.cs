b530b734e0da48c04febab57ae6090d22ce39174
Keith Becker <keith.becker@recro.com>
2022-05-12 20:24:30 -0600

    Components

---
+++ src/Components/Medulla.Components.Library/Component1.razor.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Components;
+
+namespace Medulla.Components.Library;
+
+/// <summary>
+/// This component tests
+/// </summary>
+[IsComponent("TestComponent", "This is a test component")]
+public partial class Component1
+{
+
+    /// <summary>
+    /// This field just tests a parameter in the blazor html
+    /// </summary>
+    [Parameter]
+    public string Test { get; set; } = "Test";
+
+}
---
+++ src/Components/Medulla.Components.Library/IsComponent.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+namespace Medulla.Components.Library;
+
+/// <summary>
+/// IsComponent lets the Medulla system know that the class will be pulled into the Medulla Editor as a component.
+/// </summary>
+[System.AttributeUsage(System.AttributeTargets.Class |
+                       System.AttributeTargets.Struct)]
+public class IsComponent : System.Attribute
+{
+    /// <summary>
+    /// Version of component
+    /// </summary>
+    public double version;
+
+    /// <summary>
+    /// Name of this component
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Description of this component
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Constructor for component
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="description"></param>
+    public IsComponent(string name, string description)
+    {
+        version = 1.0;
+        this.Name = name;
+        this.Description = description;
+    }
+}
